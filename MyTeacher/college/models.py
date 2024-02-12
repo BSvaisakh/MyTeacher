10062f8af1ad37cd93f50c12a85f993988a985ba
Ysakh <2002vaisakhbs@gmail.com>
2024-02-12 22:33:25 +0530

    college model creation and college login successful

--- MyTeacher/college/models.py
+++ MyTeacher/college/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class College(models.Model):
+    name = models.CharField(max_length = 50)
+    designation = models.CharField(max_length = 50)
+    email = models.EmailField(max_length = 100)
+    password = models.CharField(max_length = 50)
+    pic = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
--- MyTeacher/college/views.py
+++ MyTeacher/college/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
     return render(request,"home.html")
+
+def collegeLogin(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        
+        college = College.objects.filter(name=name).first()
+        if college is not None :
+            stored_pswd = college.password
+            if password == stored_pswd :
+                return render(request,"college_dashboard.html")
+            response = f"""
+                <script>
+                window.alert('WRONG PASSWORD')
+                window.location.href ='/';
+                </script>
+                """
+            return HttpResponse(response)
+        response = f"""
+                <script>
+                window.alert('No such User')
+                window.location.href ='/';
+                </script>
+                """
+        return HttpResponse(response)
+    return render(request,"home.html")
+            
+        
